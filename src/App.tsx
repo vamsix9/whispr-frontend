3d0776c0d781293dc16b675da26017155e90da4c
vamsix9 <bramhanapalli.vamsi@gmail.com>
2025-10-12 22:00:57 +0530

    [Vamsi] - added cicd pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Signup } from './component/Signup/Signup'
 
 function App() {
   return (
-    <>
-      <h1>Whispr</h1>
-    </>
+    <h1 className="text-3xl font-bold text-blue-500 underline">
+      Hello Tailwind v4!
+    </h1>
   )
 }
 
